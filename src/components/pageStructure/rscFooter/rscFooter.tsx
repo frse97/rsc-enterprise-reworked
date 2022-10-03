0db89422f1e7d49f27304444a01fd271a6459192
sebastianfries@outlook.com <s.fries@wbi.at>
2022-10-04 00:08:54 +0200

    Added github pages.

--- src/components/pageStructure/rscFooter/rscFooter.tsx
+++ src/components/pageStructure/rscFooter/rscFooter.tsx
@@ -1,22 +1,15 @@
-import {
-  faInstagram,
-  faLinkedin,
-  faMedium,
-  faTwitter,
-} from "@fortawesome/free-brands-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { memo } from "react";
 import "./RscFooter.scss";
 
 const RscFooter: React.FC = () => {
   return (
     <footer className="rsc-footer">
-      <div className="connections">
+      {/* <div className="connections">
         <FontAwesomeIcon icon={faLinkedin} />
         <FontAwesomeIcon icon={faTwitter} />
         <FontAwesomeIcon icon={faInstagram} />
         <FontAwesomeIcon icon={faMedium} />
-      </div>
+      </div> */}
     </footer>
   );
 };
