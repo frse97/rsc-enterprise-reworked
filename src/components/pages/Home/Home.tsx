7b26d15265eb4aa983ebe2c7bfce62a6c392b79d
sebastianfries@outlook.com <s.fries@wbi.at>
2022-10-04 22:13:10 +0200

    Adjustments home background.

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,11 +1,11 @@
 import { memo } from "react";
-import { RscContainer, RscParticles } from "../../general";
+import { RscParticles } from "../../general";
 import "./Home.scss";
 import { RscSteps, rscStepsStructure } from "../../general/RscSteps";
 import { homeParticlesOptions } from "./Home.data";
 import { RscButton } from "../../general/button";
 import { rscRoutes } from "../../shared";
-import { RscLinearGradientText, RscText } from "../../general/text";
+import { RscText } from "../../general/text";
 interface IHomeMainBackground {
   /**
    * Identifier if the home background should have particles
@@ -33,33 +33,6 @@ const HomeMainBackground: React.FC<IHomeMainBackground> = (props) => {
         {hasParticles && <RscParticles options={homeParticlesOptions} />}
       </div>
       <div className="rsc-main-cta">
-        <RscContainer
-          type="section"
-          content={
-            <RscText
-              type="h2"
-              fontSize="24px"
-              textTransform="uppercase"
-              content={
-                <>
-                  The{" "}
-                  <RscLinearGradientText
-                    type="span"
-                    fontSize="24px"
-                    fontWeight="bolder"
-                    angle={45}
-                    opacity={80}
-                    color1={"#0015ff"}
-                    color2={"#00dbce"}
-                    content={<>blockchain technology</>}
-                  />{" "}
-                  is revolutionizing the way we do business
-                </>
-              }
-            />
-          }
-        />
-
         <RscButton
           label="Discover our services"
           size="lg"
